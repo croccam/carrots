17311b49f123ea4e48e66cc943a7cc9492c673fb
Antonio Carrillo <acarrillo@ximdex.com>
2017-03-16 16:08:48 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='carrots',
+      version='0.1',
+      description='Library to build RabbitMQ architectures like you were playing with lego blocks',
+      url='http://github.com/croccam/carrots',
+      author='Croccam',
+      author_email='antcarri@gmail.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
