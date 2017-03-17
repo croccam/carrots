5a46ed452b0523f5fb491cb1030c65dec813dfb1
Antonio Carrillo <acarrillo@ximdex.com>
2017-03-17 10:06:39 +0100

    Updated example

--- examples/pipe/task_creator.py
+++ examples/pipe/task_creator.py
@@ -1,8 +1,10 @@
 from carrots import Microproducer
-
+import datetime
 rabbit_uri = 'amqp://rabbitmq:rabbitmq@localhost:5672/%2F'
 
 mp = Microproducer(rabbit_uri,'','example.tasks')
-ids = [{'id': i} for i in range(0,100)]
+ids = [{'id': i} for i in range(0,100000)]
 
+before = datetime.datetime.now()
 mp.send(ids)
+print((datetime.datetime.now()-before).total_seconds())
