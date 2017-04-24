72180accff12167fbfc8f2f055889325e2c2e95d
Antonio Carrillo <acarrillo@ximdex.com>
2017-04-24 12:13:20 +0200

    Updating setup

---
+++ carrots/common/__init__.py
@@ -0,0 +1,1 @@
+from .microclient import *
--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(name='carrots',
-      version='0.1.5',
+      version='0.1.11',
       description='Library to build RabbitMQ architectures like you were playing with lego blocks',
       url='http://github.com/croccam/carrots',
       author='Croccam',
       author_email='antcarri@gmail.com',
       license='MIT',
-      packages=['carrots'],
+      install_requires=required,
+      packages=['carrots',
+                'carrots.common'],
       zip_safe=False)
